086537284faa3e9ba36835b80ecf0b586c074514
AliAsad <fluffycookie42@gmail.com>
2022-09-28 16:53:14 -0400

    turtle

---
+++ testturtle.py
@@ -0,0 +1,10 @@
+import turtle               
+wn = turtle.Screen()        
+alex = turtle.Turtle()      
+alex.forward(150)           
+alex.left(90)               
+alex.forward(75)  
+import turtle               # allows us to use the turtles library
+wn = turtle.Screen()        # creates a graphics window
+alex = turtle.Turtle()      # create a turtle named alex
+alex.forward(150)           # tell alex to move forward by 150 unit
